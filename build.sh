bc98a8091eddf876a04e686436197dd47340b13c
Tyler Prete <psyonic@gmail.com>
2010-05-28 14:31:40 -0600

    Added scripts to build Go (used to run everything) and modified runghci.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-langdir=Language/Scheme/R5RS
+langdir=src/Language/Scheme/R5RS
 parsedir=$langdir/Parser
 syntaxdir=$langdir/Syntax
 transformerdir=$langdir/Transformer
 alex -g $parsedir/Lexer.x # -o Lexer.hs
 happy -g -a -c $parsedir/Parser.y # -o Parser.hs
-ghci Parser.hs $syntaxdir/Expression.hs Lexer.hs $parsedir/Token.hs $parsedir/ParserUtils.hs $syntaxdir/PrettyPrint.hs
+ghc -isrc --make src/Go -o Go

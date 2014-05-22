a52c265082d65ba83ec5f6217dd1329b09fb2890
Tyler Prete <tyler@radius.com>
2014-05-21 22:42:32 -0700

    Rename generated executable.

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ syntaxdir=$langdir/Syntax
 transformerdir=$langdir/Transformer
 alex -g $parsedir/Lexer.x # -o Lexer.hs
 happy -g -a -c $parsedir/Parser.y # -o Parser.hs
-ghc -isrc --make src/Main -o darwin
+ghc -isrc --make src/Main -o tylerscheme
